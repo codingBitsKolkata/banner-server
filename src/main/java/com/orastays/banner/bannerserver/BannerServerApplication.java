e05f3fb9081459ea4c99b5eb65b10b4dc815c95e
Akshay <akshayball01@gmail.com>
2018-12-07 10:46:37 +0530

    first commit

---
+++ src/main/java/com/orastays/banner/bannerserver/BannerServerApplication.java
@@ -0,0 +1,12 @@
+package com.orastays.banner.bannerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerServerApplication.class, args);
+	}
+}
