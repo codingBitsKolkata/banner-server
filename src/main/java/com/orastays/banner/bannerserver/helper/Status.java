b645656c1848ee29fe831b9d124cc68f7608ce40
Avirup <avirup.pal@gmail.com>
2019-02-02 21:59:10 +0530

    package names changes

--- src/main/java/com/orastays/banner/bannerserver/helper/Status.java
+++
@@ -1,6 +0,0 @@
-package com.orastays.banner.bannerserver.helper;
-
-public enum Status {
-
-	ZERO, ACTIVE, INACTIVE, DELETE
-}
--- src/main/java/com/orastays/banner/bannerserver/service/DatabaseService.java
+++
@@ -1,7 +0,0 @@
-package com.orastays.banner.bannerserver.service;
-
-public interface DatabaseService {
-	
-	public abstract String databaseOperation(String query) throws Exception ;
-	
-}
